a51abdcb2a9c88e131893b7dcecaa04b4f019ba2
Lucrecia Garcia <lu_90@hotmail.com>
2023-07-09 12:54:18 -0300

    segunda entrega

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,9 +1,15 @@
-
+import Item from "./Item";
 
 const ItemList = ( {productos} ) => {
-    console.log(productos);
+
     return (
-        <div>ItemList</div>
+        <div className="conteiner">
+            <h2 className="main-title">Productos</h2>
+
+            <div className="productos">
+                { productos.map((prod) => <Item producto={prod} key={prod.id} />) }
+            </div>
+        </div>
     )
 }
 
